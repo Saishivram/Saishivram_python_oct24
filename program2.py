81d005f11abd4d6bd22d22b5df2d471f4391fa3c
Saishivram V <saishivramv@gmail.com>
2024-10-21 17:53:27 +0530

    Delete program2.py

--- program2.py
+++
@@ -1,7 +0,0 @@
-# program to check if a number is even.
-print("Enter a number to check if it is Even:")
-input_num=int(input())
-if input_num%2==0:
-    print('Input number',input_num,'is Even')
-else:
-    print('Input number',input_num,'is not Even')
